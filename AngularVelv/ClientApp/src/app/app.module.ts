8b025593d16f2e99214e512d26d92444d0f4d1c4
pako2 <deacle2009@mail.ru>
2020-08-28 09:46:18 +0700

    create material-module.ts

--- AngularVelv/ClientApp/src/app/app.module.ts
+++ AngularVelv/ClientApp/src/app/app.module.ts
@@ -11,16 +11,7 @@ import { StudentComponent } from './student/student.component';
 import { GroupComponent } from './group/group.component';
 import { StudentgroupComponent } from './studentgroup/studentgroup.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSelectModule } from '@angular/material/select';
-import { MatRadioModule } from '@angular/material/radio';
-import { MatCardModule } from '@angular/material/card';
-import { ReactiveFormsModule } from '@angular/forms';
-import { MatTableModule } from '@angular/material/table';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatSortModule } from '@angular/material/sort';
-import { MatSliderModule } from '@angular/material/slider';
+import { MaterialModule } from '../shared/material-module';
 
 @NgModule({
   declarations: [
@@ -35,16 +26,7 @@ import { MatSliderModule } from '@angular/material/slider';
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
-    MatSliderModule,
-    MatInputModule,
-    MatButtonModule,
-    MatSelectModule,
-    MatRadioModule,
-    MatCardModule,
-    ReactiveFormsModule,
-    MatTableModule,
-    MatPaginatorModule,
-    MatSortModule,
+    MaterialModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'students', component: StudentComponent },
